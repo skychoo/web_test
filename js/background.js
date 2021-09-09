554223426a7a252dde9bc19a60f691af6c066d0d
skychoo <bitchoo@naver.com>
2021-09-09 16:13:02 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+//const bgImage = document.createElement("img");
 
-bgImage.src = `imgs/${chosenImage}`;
-document.body.appendChild(bgImage);
+//bgImage.src = `img/${chosenImage}`;
+
+//document.body.appendChild(bgImage);
+document.body.style = `background-image:url(img/${chosenImage}); background-size:cover;`;
